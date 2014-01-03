2619b282bf927a429c58865ef954aa2cfeb5fbd8
akoskenalho <antti.koskenalho@gmail.com>
2014-01-03 22:59:40 +0200

    Empty template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JKino::Application.config.secret_key_base = 'c8026cf8f9c381323f58bd7ed30fd01192fb4fd19f5b419c0aef0bc33dce894d8cb83188a330f58fe8dafd2fac02408e98976dcc4d143dac6e5f469c183ffda6'
