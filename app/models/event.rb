c59e9b440c39bdde2c217a1c2b8237d352a8642a
akoskenalho <antti.koskenalho@gmail.com>
2014-01-12 19:22:34 +0200

    Validation done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
 	has_and_belongs_to_many :movies
+	
+	validates :event_date, presence: true
+	validates :event_name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
 	has_and_belongs_to_many :events
+
+	validates :movie_name, presence: true
 end
