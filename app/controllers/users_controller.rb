a2c48b934a720d5bf5bc6197c6da36441dfc70c7
soltiger <antti.koskenalho@gmail.com>
2014-01-29 19:14:36 +0200

    Added some seed data for testing purposes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 # encoding: utf-8
 
 class UsersController < ApplicationController
-  def new
-	@user = User.new
-  end
+	def new
+		@user = User.new
+	end
   
-  def create
+	def create
 		if admin_rights
 			@user = User.new( user_params )
-				if @user.save
+			
+			if @user.save
 				redirect_to root_url, :notice => "Käyttäjä luotu"
 			else
 				render "new"
 			end
-	  end
+		end
 	end
   
 	private
